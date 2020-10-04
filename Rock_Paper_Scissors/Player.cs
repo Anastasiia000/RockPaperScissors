2b0414d44ac3ac566d2827748226f6bc7e0332f4
Anastasiia000 <57679919+Anastasiia000@users.noreply.github.com>
2020-10-04 21:04:41 +0300

    full projekt

---
+++ Rock_Paper_Scissors/Player.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class Player
+    {
+        public string name;
+        public VARIANTS VARIANTS;
+        private Random rnd = new Random();
+        private int variants;
+
+        public Player(VARIANTS variants, string name)
+        {
+            this.name = name;
+            this.VARIANTS = variants;
+        }
+        public Player()
+        {
+            variants = rnd.Next(1, 3);
+            switch (variants)
+            {
+                case 1: VARIANTS = VARIANTS.Rock; break;
+                case 2: VARIANTS = VARIANTS.Scissors; break;
+                case 3: VARIANTS = VARIANTS.Paper; break;
+            }
+
+            name = "Bot";            
+        }
+
+        public string whoWins(Player player1, Player player2)
+        {
+            string winner = "Winner is ";
+            if (player1.VARIANTS == VARIANTS.Paper && player2.VARIANTS == VARIANTS.Paper)
+                winner = "Draw";
+            if (player1.VARIANTS == VARIANTS.Rock && player2.VARIANTS == VARIANTS.Rock)
+                winner = "Draw";
+            if (player1.VARIANTS == VARIANTS.Scissors && player2.VARIANTS == VARIANTS.Scissors)
+                winner = "Draw";
+            if (player1.VARIANTS == VARIANTS.Rock && player2.VARIANTS == VARIANTS.Paper)
+                winner += (string)player2.name;
+            if (player1.VARIANTS == VARIANTS.Paper && player2.VARIANTS == VARIANTS.Rock)
+                winner += (string)player1.name;
+            if (player1.VARIANTS == VARIANTS.Paper && player2.VARIANTS == VARIANTS.Scissors)
+                winner += (string)player2.name;
+            if (player1.VARIANTS == VARIANTS.Scissors && player2.VARIANTS == VARIANTS.Paper)
+                winner += (string)player1.name;
+            if (player1.VARIANTS == VARIANTS.Rock && player2.VARIANTS == VARIANTS.Scissors)
+                winner += (string)player1.name;
+            if (player1.VARIANTS == VARIANTS.Scissors && player2.VARIANTS == VARIANTS.Rock)
+                winner += (string)player2.name;
+            return winner;
+        }
+
+    }
+}
---
+++ Rock_Paper_Scissors/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_Scissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player bot = new Player();
+            Player alex = new Player(VARIANTS.Paper, "alex");
+            Console.WriteLine($"name: {alex.name}, var: {alex.VARIANTS}");
+            Console.WriteLine($"name: {bot.name}, var: {bot.VARIANTS}");
+
+            Console.WriteLine(bot.whoWins(bot, alex));
+
+            Console.ReadKey();
+        }
+    }
+}
